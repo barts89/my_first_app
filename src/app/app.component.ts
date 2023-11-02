26667a2f159258e331d9027faf7478f672701893
barts89 <bartpadovano@gmail.com>
2023-11-02 18:08:13 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  styles: ['h2 {color: yellow;}']
+})
+export class AppComponent {
+  name = 'Bartolo';
+}
